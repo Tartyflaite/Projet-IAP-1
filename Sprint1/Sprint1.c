a7ba720f2e3c48f26dd318d685165b531533f1e5
Tartyflaite <71936440+Tartyflaite@users.noreply.github.com>
2020-09-29 15:20:43 +0200

    Update Sprint1.c
    ajout des fonctions embauche client tache progression

--- Sprint1/Sprint1.c
+++ Sprint1/Sprint1.c
@@ -9,6 +9,10 @@ Booleen EchoActif = FAUX;
 // Messages emis par les instructions -----------------------------------------
 #define MSG_DEVELOPPE "## nouvelle specialite \"%s \" ; cout horaire \"%d\"\n" 
 #define MSG_INTERRUPTION "## fin de programme\n" 
+#define MSG_EMBAUCHE "## nouveau travailleur \"%s\" competent pour la specialite \"%s\"\n"
+#define MSG_CLIENT "## nouveau client \"%s\"\n"
+#define MSG_TACHE "## la commande \"%s\" requiere la specialite \"%s\" (nombre d’heures \"%d\")\n"
+#define MSG_PROGRESSION "## pour la commande \"%s\", pour la specialite \"%s\" : \"%d\" heures de plus ont ete realisees\n"
 
 // Lexemes -------------------------------------------------------------------- 
 #define LGMOT 35
@@ -26,6 +30,38 @@ int get_int() {
 }
 
 // Instructions --------------------------------------------------------------- 
+
+// Progression -------------------------
+void traite_progression() {
+	Mot nom_commade, nom_specialite;
+	get_id(nom_commade);
+	get_id(nom_specialite);
+	int nbr_heure = get_int;
+	printf(MSG_PROGRESSION, nom_commade, nom_specialite, nbr_heure);
+}
+
+// Tâches ------------------------------
+void traite_tache() {
+	Mot nom_commande, nom_specialite;
+	getid(nom_commande);
+	get_id(nom_specialite);
+	int nbr_heure = get_int();
+	printf(MSG_TACHE,nom_commande,nom_specialite,nbr_heure);
+}
+
+// Client ------------------------------
+void traite_client() {
+	Mot nom_client;
+	get_id(nom_client);
+	printf(MSG_CLIENT, nom_client);
+}
+// Embauche ----------------------------
+void traite_embauche() {
+	Mot nom_travailleur, nom_specialite;
+	get_id(nom_travailleur);
+	get_id(nom_specialite);
+	printf(MSG_EMBAUCHE, nom_travailleur, nom_specialite);
+}
 // developpe --------------------------- 
 void traite_developpe() {
 	Mot nom_specialite;
